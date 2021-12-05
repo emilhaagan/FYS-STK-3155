13159b2cc2a11905df28a8b2df549bab5bbf6698
emilhaagan <emil.haagan@gmail.com>
2021-12-05 14:57:05 +0100

    Testing

--- Project3/SRC/GradientBoosting.py
+++ Project3/SRC/GradientBoosting.py
@@ -3,25 +3,29 @@
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from NN import Loss_CC
+#from NN import Loss_CC
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_validate
+
 
 
 class GradientBoosting():
 
-    def __init__(self, n_epoch, learningrate, X, y):
+    def __init__(self, n_epoch, learningrate):
         self.n_epoch = n_epoch
         self.learningrate = learningrate
-        self.X = X
-        self.y = y
 
 
         self.trees = []
         for i in range(n_epoch):
-            self.tress.append(DecisionTreeClassifier(X, y))
+            self.trees.append(DecisionTreeClassifier())
 
 
-    def fit(self):
+    def fit(self, X, y):
+        y_predictict = np.full(np.shape(y), np.mean(y, axis=0))
         for i in range(self.n_epoch):
             grad = self.grad(y, y_predictict)
             self.trees[i].fit(X, grad)
@@ -45,3 +49,27 @@ class GradientBoosting():
         predict = np.clip(predict, 1e-14, 1-1e-14)
         r = y/predict + (1-y)/(1-predict)
         return r
+
+
+
+'''Test Wisconsin Cancer Data'''
+data = load_breast_cancer()
+X = data['data']
+y = data['target']
+
+#Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+
+#Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+clf = GradientBoosting(n_epoch = 100, learningrate=0.1)
+
+clf.fit(X_train, y_train)
+#Cross validation
+accuracy = cross_validate(clf, X_test, y_test,cv=10)['test_score']
+print(accuracy)
